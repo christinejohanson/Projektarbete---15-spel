3511858f22b94286af39e1b2547ff48013e136cb
christinejohanson <christine.johanson@yahoo.com>
2023-10-15 16:04:10 +0200

    refactoring code

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,6 +1,6 @@
 (function () {
-    //globally variabels
-    var overlay, sizeInput; 
+    //globally variabels and el to show that they are part of dom.
+    var overlayEl, sizeInputEl, boardEl, movesTextEl, winModalEl, winMovesEl, titleNumberEl, sizeDisplayEl; 
 
     //function init
     function init() {
@@ -13,37 +13,33 @@
 
     //function to start new game
     function startNewGame() {
-        var movesText = document.getElementById("moves");
         //set count to 0
-        movesText.textContent = 0;
+        movesTextEl.textContent = 0;
         //empty board
         emptyBoard();
         //take the input size
         var size = sizeInputEl.value;
         //generate board
         generateBoard(size);
-        //update headline w correct size
-        document.getElementById("titleNumber").innerText = (size * size) - 1;
+        //update heading w correct size
+        titleNumberEl.innerText = (size * size) - 1;
     }
 
     function emptyBoard() {
-        var board = document.getElementById("board");
-        //if there is child elements
-        while (board.firstChild) {
-            board.firstChild.remove();
+        //empty board if there is child elements
+        while (boardEl.firstChild) {
+            boardEl.firstChild.remove();
         }
     }
 
     //function to generate board with parameter size
     function generateBoard(size) {
-        var board = document.getElementById("board");
         //create variable for the tiles
         var tiles;
         //loop to see if it is solvable
         do {
             tiles = generateTiles(size);
         } while (!isSolvable(tiles) || isSolved(tiles));
-        // var tiles = generateTiles(size);
         //for loop to create tr and td
         for (var i = 0; i < size; i++) {
             //tr
@@ -66,119 +62,27 @@
                 outputRow.appendChild(outputTile);
             }
             //append row to board
-            board.appendChild(outputRow);
+            boardEl.appendChild(outputRow);
         }
         console.log(tiles + "tiles");
     }
 
-    //function for random number on the tiles
-    function generateTiles(size) {
-        //empty array
-        var randomArray = [];
-        //fill the array with right amount of numbers
-        var max = size * size;
-        //while loop that runs when less than max
-        while (randomArray.length < max) {
-            var randomNumber = Math.floor(Math.random() * max);
-            //if to see if number is already in array
-            if (randomArray.indexOf(randomNumber) === -1) {
-                //add to array
-                randomArray.push(randomNumber);
-            }
-        }
-        //create a twodimensional array
-        var tileArray = new Array(size);
-        for (var i = 0; i < size; i++) {
-            tileArray[i] = new Array(size);
-        }
-
-        //add random numbers in the 2d array
-        var tileCount = 0;
-        for (i = 0; i < size; i++) {
-            for (var j = 0; j < size; j++) {
-                tileArray[i][j] = randomArray[tileCount];
-                tileCount++;
-            }
-        }
-        return tileArray;
-    }
-
-    //function for game size and new game
-    function setupControls() {
-        var startButton = document.getElementById("startGame");
-        startButton.addEventListener("click", startNewGame);
-        //size of the board
-        sizeInputEl = document.getElementById("boardSize");
-        //create eventlistener to look for input
-        sizeInputEl.addEventListener("input", function () {
-            //get the span element
-            var sizeDisplay = document.getElementById("currentSize");
-            sizeDisplay.innerHTML = this.value + " &times " + this.value;
-        });
-    }
-
-    //modal functions
-    function setupModal() {
-        //new div that is the background
-        overlay = document.createElement("div");
-        overlay.className = "overlay";
-        var restartButton = document.getElementById("restartGame");
-        //event listener on button
-        restartButton.addEventListener("click", function () {
-            closeModal();
-            startNewGame();
-        });
-    }
-
-    //change board with move error
-    function setupBoard() {
-        //variable for table board
-        var board = document.getElementById("board");
-        //eventlistener that listens to moveerror
-        board.addEventListener("moveError", function () {
-            //create element variable instead of only this
-            var el = this;
-            //add class
-            el.className = "wrong-move";
-            //remove classname after 200 millisec
-            setTimeout(function () {
-                el.className = "";
-            }, 200);
-        });
-    }
-
-    function openModal() {
-        var winModal = document.getElementById("winModal");
-        //remove class offscreen
-        winModal.classList.remove("off-screen");
-        //element that covers everything behind modal window
-        document.body.appendChild(overlay);
-    }
-
-    function closeModal() {
-        var winModal = document.getElementById("winModal");
-        //add class offscreen
-        winModal.classList.add("off-screen");
-        //remove background
-        document.body.removeChild(overlay);
-    }
-     
     //function for move the tiles
     function moveThisTile() {
         //find the cell position
         var column = this.cellIndex;
         var row = this.parentNode.rowIndex;
         //find the hole position
-        var hole = document.querySelector(".blank");
-        var holeColumn = hole.cellIndex;
-        var holeRow = hole.parentNode.rowIndex;
+        var holeEl = document.querySelector(".blank");
+        var holeColumn = holeEl.cellIndex;
+        var holeRow = holeEl.parentNode.rowIndex;
         //compare the two values from row and column
         var diffCols = holeColumn - column;
         var diffRows = holeRow - row; 
         //check if the difference is 1
         if ((Math.abs(diffRows) === 1 && diffCols === 0) || (Math.abs(diffCols) === 1 && diffRows === 0)) {
             //calling function
-            swapElements(this, hole);
+            swapElements(this, holeEl);
             incrementMoves();
         }
         else {
@@ -216,15 +120,14 @@
 
     //to count the moves
     function incrementMoves() {
-        var movesText = document.getElementById("moves");
-        var moves = parseInt(movesText.textContent);
+        var moves = parseInt(movesTextEl.textContent);
         moves++;
-        movesText.textContent = moves;
+        movesTextEl.textContent = moves;
 
         //check if all bricks are right
         if (checkIfWinner()) {
             //add moves to the winmoves
-            document.getElementById("winMoves").textContent = moves;
+            winMovesEl.textContent = moves;
             //call the function
             openModal();
         }
@@ -233,12 +136,12 @@
     //check the bricks in right position
     function checkIfWinner() {
         //get the dom element
-        var board = document.getElementById("board");
-        var size = board.rows.length;
+        // var board = document.getElementById("board");
+        var size = boardEl.rows.length;
         //create loop to check the numbers one by one
         var count = 1;
         //get all the tiles
-        var tiles = board.getElementsByTagName("td");
+        var tiles = boardEl.getElementsByTagName("td");
         //while loop
         while (count < tiles.length){
             if(tiles[count - 1].textContent != count){
@@ -251,16 +154,109 @@
 
     }
 
-    //test för olika random siffror med avrundning uppåt o neråt
-    // var randNum = Math.random(); 
-    // console.log(randNum); 
-    // var max = 16;
-    // var randIntFloor = Math.floor(randNum * max);
-    // console.log(randIntFloor);
-    // var randIntCeil = Math.ceil(randNum * max);
-    // console.log(randIntCeil);
+    //function for random number on the tiles
+    function generateTiles(size) {
+        //empty array
+        var randomArray = [];
+        //fill the array with right amount of numbers
+        var max = size * size;
+        //while loop that runs when less than max
+        while (randomArray.length < max) {
+            var randomNumber = Math.floor(Math.random() * max);
+            //if to see if number is already in array
+            if (randomArray.indexOf(randomNumber) === -1) {
+                //add to array
+                randomArray.push(randomNumber);
+            }
+        }
+        //create a twodimensional array
+        var tileArray = new Array(size);
+        for (var i = 0; i < size; i++) {
+            tileArray[i] = new Array(size);
+        }
+
+        //add random numbers in the 2d array
+        var tileCount = 0;
+        for (i = 0; i < size; i++) {
+            for (var j = 0; j < size; j++) {
+                tileArray[i][j] = randomArray[tileCount];
+                tileCount++;
+            }
+        }
+        return tileArray;
+    }
+
+    //change board with move error
+    function setupBoard() {
+        //variable for table board
+        boardEl = document.getElementById("board");
+        //eventlistener that listens to moveerror
+        boardEl.addEventListener("moveError", function () {
+            //create element variable instead of only this
+            var el = this;
+            //add class
+            el.className = "wrong-move";
+            //remove classname after 200 millisec
+            setTimeout(function () {
+                el.className = "";
+            }, 200);
+        });
+    }
+
+    //function for game size and new game
+    function setupControls() {
+        var startButtonEl = document.getElementById("startGame");
+        //eventlisternet to start new game
+        startButtonEl.addEventListener("click", startNewGame);
+        //element that shows all the moves
+        movesTextEl = document.getElementById("moves");
+        //number in the title
+        titleNumberEl = document.getElementById("titleNumber");
+        //input element on size of board
+        sizeInputEl = document.getElementById("boardSize");
+        //span element
+        sizeDisplayEl = document.getElementById("currentSize");
+        //create eventlistener to look for input
+        sizeInputEl.addEventListener("input", function () {
+            sizeDisplayEl.innerHTML = this.value + " &times " + this.value;
+        });
+    }
+
+    //modal functions
+    function setupModal() {
+        //div element
+        winModalEl = document.getElementById("winModal");
+        //moves to solution
+        winMovesEl = document.getElementById("winMoves");
+        // board = document.getElementById("board");
+        //new div that is the background
+        overlayEl = document.createElement("div");
+        overlayEl.className = "overlay";
+        var restartButton = document.getElementById("restartGame");
+        //event listener on button
+        restartButton.addEventListener("click", function () {
+            closeModal();
+            startNewGame();
+        });
+    }
+
+    //show the modal
+    function openModal() {
+        //remove class offscreen
+        winModalEl.classList.remove("off-screen");
+        //element that covers everything behind modal window
+        document.body.appendChild(overlayEl);
+    }
+
+    //hide the modal
+    function closeModal() {
+        //add class offscreen
+        winModalEl.classList.add("off-screen");
+        //remove background
+        document.body.removeChild(overlayEl);
+    }
+     
     //run init when window is loaded
     window.addEventListener("load", init);
-
 
 })();
